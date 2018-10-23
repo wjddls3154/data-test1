b65227b73b0699c22a5865034e421c1b8d31a157
kwackjihoon <chunjoo4@naver.com>
2018-10-23 10:56:35 +0900

    BookData class create

---
+++ data-test1/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  data-test1
+//
+//  Created by dit on 2018. 10. 23..
+//  Copyright © 2018년 dit. All rights reserved.
+//
+
+import Foundation
+class BookData {
+    var title = ""
+    var author = ""
+}
